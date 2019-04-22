816f28fa937c2ae2f7d8dda325a8b6337b00c5b0
Chonim <chihoyoon1217@gmail.com>
2019-04-22 15:11:54 +0900

    solve averagePair problem

--- src/algorithms/exercises/problem-solving-patterns/multiple-pointers/average-pair.js
+++ src/algorithms/exercises/problem-solving-patterns/multiple-pointers/average-pair.js
@@ -1,3 +1,17 @@
-function averagePair () {
-  // add whatever parameters you deem necessary - good luck!
+function averagePair (arr, targetAverage) {
+  let left = 0
+  let right = arr.length - 1
+  while (left < right) {
+    const average = (arr[left] + arr[right]) / 2
+    if (average === targetAverage) {
+      return true
+    } else if (average > targetAverage) {
+      right--
+    } else {
+      left++
+    }
+  }
+  return false
 }
+
+module.exports = averagePair
