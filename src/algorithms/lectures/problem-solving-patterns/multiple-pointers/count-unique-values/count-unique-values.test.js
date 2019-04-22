a0a1394fd755edbbd6ba6d4fc9b309d743fca6e6
Chonim <chihoyoon1217@gmail.com>
2019-04-22 10:56:21 +0900

    modify test descriptions

--- src/algorithms/lectures/problem-solving-patterns/multiple-pointers/count-unique-values/count-unique-values.test.js
+++ src/algorithms/lectures/problem-solving-patterns/multiple-pointers/count-unique-values/count-unique-values.test.js
@@ -1,14 +1,14 @@
 const countUniqueValues = require('./count-unique-values')
 const countUniqueValues2 = require('./count-unique-values')
 
-it('counts unique values', () => {
+it('counts the number of unique values', () => {
   expect(countUniqueValues([1, 1, 1, 1, 2])).toBe(2)
   expect(countUniqueValues([1, 2, 3, 4, 4, 7, 12, 12, 13])).toBe(7)
   expect(countUniqueValues([])).toBe(0)
   expect(countUniqueValues([-2, -1, -1, 0, 1])).toBe(4)
 })
 
-it('counts unique values', () => {
+it('counts the number of unique values', () => {
   expect(countUniqueValues2([1, 1, 1, 1, 2])).toBe(2)
   expect(countUniqueValues2([1, 2, 3, 4, 4, 7, 12, 12, 13])).toBe(7)
   expect(countUniqueValues2([])).toBe(0)
--- src/algorithms/lectures/problem-solving-patterns/multiple-pointers/multiple-pointers.test.js
+++ src/algorithms/lectures/problem-solving-patterns/multiple-pointers/multiple-pointers.test.js
@@ -1,6 +1,6 @@
 const sumZero = require('./multiple-pointers')
 
-it('finds a pair that makes 0', () => {
+it('finds the first pair that makes 0', () => {
   expect(sumZero([-3, -2, -1, 0, 1, 2, 3])).toEqual([-3, 3])
   expect(sumZero([-2, 0, 1, 3])).toEqual(undefined)
   expect(sumZero([1, 2, 3])).toEqual(undefined)
