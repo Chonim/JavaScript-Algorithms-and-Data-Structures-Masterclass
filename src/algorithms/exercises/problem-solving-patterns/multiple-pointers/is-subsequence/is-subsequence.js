8274c0553be043227b998c10ff4e354b88c5f53c
Chonim <chihoyoon1217@gmail.com>
2019-04-22 15:51:10 +0900

    refactor isSubsequence

--- src/algorithms/exercises/problem-solving-patterns/multiple-pointers/is-subsequence/is-subsequence.js
+++ src/algorithms/exercises/problem-solving-patterns/multiple-pointers/is-subsequence/is-subsequence.js
@@ -1,13 +1,11 @@
 function isSubsequence (str1, str2) {
   let i = 0
-  let trueCount = 0
   for (let j = 0; j < str2.length; j++) {
     if (str1[i] === str2[j]) {
-      trueCount++
       i++
     }
   }
-  return trueCount === str1.length
+  return i === str1.length
 }
 
 module.exports = isSubsequence
