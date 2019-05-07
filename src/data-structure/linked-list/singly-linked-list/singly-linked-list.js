9cbb5e7058f95b8ab8c2d2645998d5fd8b3a476d
Chonim <chihoyoon1217@gmail.com>
2019-05-07 10:52:09 +0900

    finish singly linked list

--- src/data-structure/linked-list/singly-linked-list/singly-linked-list.js
+++ src/data-structure/linked-list/singly-linked-list/singly-linked-list.js
@@ -45,16 +45,109 @@ class SinglyLinkedList {
     }
     return current
   }
+  shift () {
+    if (!this.head) {
+      return undefined
+    }
+    const currentHead = this.head
+    this.head = currentHead.next
+    this.length--
+    if (!this.length) {
+      this.head = null
+      this.tail = null
+    }
+    return currentHead
+  }
+  unshift (val) {
+    const node = new Node(val)
+    if (!this.head) {
+      this.head = node
+      this.tail = this.head
+    } else {
+      node.next = this.head
+      this.head = node
+    }
+    this.length++
+    return this
+  }
+  get (index) {
+    if (index < 0 || index >= this.length) {
+      return null
+    }
+    let count = 0
+    let currentNode = this.head
+    while (count < index) {
+      currentNode = currentNode.next
+      count++
+    }
+    return currentNode
+  }
+  set (index, val) {
+    const node = this.get(index)
+    if (!node) {
+      return false
+    }
+    node.val = val
+    return true
+  }
+  insert (index, val) {
+    if (index < 0 || index > this.length) {
+      return false
+    }
+    if (index === this.length) {
+      return !!this.push(val)
+    }
+    if (index === 0) {
+      return !!this.unshift(val)
+    }
+    const node = new Node(val)
+    const prevNode = this.get(index - 1)
+    const temp = prevNode.next
+    prevNode.next = node
+    node.next = temp
+    this.length++
+    return true
+  }
+  remove (index) {
+    if (index < 0 || index >= this.length) {
+      return undefined
+    }
+    if (index === this.length - 1) {
+      return this.pop()
+    }
+    if (index === 0) {
+      return this.shift()
+    }
+    const prevNode = this.get(index - 1)
+    const nodeToRemove = prevNode.next
+    prevNode.next = nodeToRemove.next
+    this.length--
+    return nodeToRemove
+  }
+  reverse () {
+    let node = this.head
+    this.head = this.tail
+    this.tail = node
+
+    let next
+    let prev = null
+    for (let i = 0; i < this.length; i++) {
+      next = node.next
+      node.next = prev
+      prev = node
+      node = next
+    }
+    return this
+  }
+  print () {
+    const arr = []
+    let current = this.head
+    while (current) {
+      arr.push(current.val)
+      current = current.next
+    }
+    console.log(arr)
+  }
 }
-
-// var first = new Node("Hi")
-// first.next = new Node("there")
-// first.next.next = new Node("how")
-// first.next.next.next = new Node("are")
-// first.next.next.next.next = new Node("you")
-
-// var list = new SinglyLinkedList()
-// list.push('HELLO')
-// list.push('GOODBYE')
 
 module.exports = SinglyLinkedList
--- src/data-structure/linked-list/singly-linked-list/singly-linked-list.test.js
+++ src/data-structure/linked-list/singly-linked-list/singly-linked-list.test.js
@@ -2,15 +2,85 @@ const SinglyLinkedList = require('./singly-linked-list')
 
 const singlyLinkedList = new SinglyLinkedList()
 
-it('pushes a node', () => {
-  expect(singlyLinkedList.push('Hi').length).toBe(1)
-  expect(singlyLinkedList.push('there').length).toBe(2)
-  expect(singlyLinkedList.push('how').length).toBe(3)
-  expect(singlyLinkedList.push('are').length).toBe(4)
-})
+describe('Linked List', () => {
+  it('pushes a node', () => {
+    expect(singlyLinkedList.push('Hi').length).toBe(1)
+    expect(singlyLinkedList.push('there').length).toBe(2)
+    expect(singlyLinkedList.push('how').length).toBe(3)
+    expect(singlyLinkedList.push('are').length).toBe(4)
+  })
 
-it('pops a node', () => {
-  expect(singlyLinkedList.pop().val).toBe('are')
-  expect(singlyLinkedList.length).toBe(3)
-  expect(singlyLinkedList.tail.val).toBe('how')
+  it('pops a node', () => {
+    expect(singlyLinkedList.pop().val).toBe('are')
+    expect(singlyLinkedList.length).toBe(3)
+    expect(singlyLinkedList.tail.val).toBe('how')
+  })
+
+  it('shifts a node', () => {
+    expect(singlyLinkedList.shift().val).toBe('Hi')
+    expect(singlyLinkedList.shift().val).toBe('there')
+    expect(singlyLinkedList.shift().val).toBe('how')
+    expect(singlyLinkedList.length).toBe(0)
+  })
+
+  it('unshifts a node', () => {
+    expect(singlyLinkedList.unshift('Yo').length).toBe(1)
+    expect(singlyLinkedList.unshift('Hey').length).toBe(2)
+  })
+
+  it('gets a node', () => {
+    expect(singlyLinkedList.push('how').length).toBe(3)
+    // Hey Yo how
+    expect(singlyLinkedList.get(3)).toBe(null)
+    expect(singlyLinkedList.get(-1)).toBe(null)
+    expect(singlyLinkedList.get(0).val).toBe('Hey')
+    expect(singlyLinkedList.get(1).val).toBe('Yo')
+    expect(singlyLinkedList.get(2).val).toBe('how')
+  })
+
+  it('sets a node', () => {
+    expect(singlyLinkedList.set(10, 'wow')).toBe(false)
+    expect(singlyLinkedList.set(1, 'man')).toBe(true)
+    expect(singlyLinkedList.get(1).val).toBe('man')
+    // Hey Yo how
+  })
+
+  it('sets a node', () => {
+    expect(singlyLinkedList.set(10, 'wow')).toBe(false)
+    expect(singlyLinkedList.set(1, 'man')).toBe(true)
+    expect(singlyLinkedList.get(1).val).toBe('man')
+    // Hey man how
+  })
+
+  it('inserts a node', () => {
+    expect(singlyLinkedList.insert(-1, 0)).toBe(false)
+    expect(singlyLinkedList.insert(10, 0)).toBe(false)
+    // Hey man how
+    expect(singlyLinkedList.insert(3, 'are')).toBe(true)
+    // Hey man how are
+    expect(singlyLinkedList.insert(2, '!')).toBe(true)
+    expect(singlyLinkedList.insert(0, 'Yo')).toBe(true)
+    // Yo Hey man ! how are
+  })
+
+  it('remove a node', () => {
+    singlyLinkedList.remove(0)
+    // Hey man ! how are
+    expect(singlyLinkedList.head.val).toBe('Hey')
+    singlyLinkedList.remove(4)
+    // Hey man ! how
+    expect(singlyLinkedList.tail.val).toBe('how')
+    expect(singlyLinkedList.remove(1).val).toBe('man')
+    expect(singlyLinkedList.length).toBe(3)
+    // Hey ! how
+  })
+
+  it('reverse the list', () => {
+    singlyLinkedList.reverse()
+    // how ! Hey
+    singlyLinkedList.print()
+    console.log(singlyLinkedList)
+    expect(singlyLinkedList.head.val).toBe('how')
+    expect(singlyLinkedList.tail.val).toBe('Hey')
+  })
 })
